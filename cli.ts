e4e3c40e9d33d9b6931151875fcee245d2987b01
Andreas Sander <mail@andi1984.de>
2020-11-16 22:08:51 +0100

    fix(cli) Typo

--- cli.ts
+++ cli.ts
@@ -2,6 +2,6 @@ import minNode from "./dev/min-node";
 export async function cli(args: string[]) {
   const minNodeVersion = await minNode().catch(console.error);
   console.log(
-    `The miasasnimum necessary Node version is at least ${minNodeVersion}.`
+    `The minimum necessary Node version is at least ${minNodeVersion}.`
   );
 }
