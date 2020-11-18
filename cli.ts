918f223b24e7eac1cc1bd8a1950aa5b95c6aff17
Andreas Sander <mail@andi1984.de>
2020-11-18 19:14:37 +0100

    chore(exports) Update source file structure for exports

--- cli.ts
+++ cli.ts
@@ -1,7 +1,7 @@
-import minNode from "./dev/min-node";
-export async function cli(args: string[]) {
-  const minNodeVersion = await minNode().catch(console.error);
-  console.log(
-    `The minimum necessary Node version is at least ${minNodeVersion}.`
-  );
-}
+import async from "./dev/async";
+import cli from "./dev/cli";
+
+export default {
+  cli,
+  async,
+};
---
+++ dev/cli.ts
@@ -0,0 +1,10 @@
+import minNode from "./async";
+
+async function cli(args: string[]) {
+  const minNodeVersion = await minNode().catch(console.error);
+  console.log(
+    `The minimum necessary Node version is at least ${minNodeVersion}.`
+  );
+}
+
+export default cli;
